32cfc44d5e0214198f1016b45b0a4a9365a8537d
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-28 20:56:52 +0700

    add program repository, service and model:

---
+++ src/main/java/com/codegym/classroom/service/program/IProgramService.java
@@ -0,0 +1,7 @@
+package com.codegym.classroom.service.program;
+
+import com.codegym.classroom.model.Program;
+import com.codegym.classroom.service.IGeneralService;
+
+public interface IProgramService extends IGeneralService<Program> {
+}
