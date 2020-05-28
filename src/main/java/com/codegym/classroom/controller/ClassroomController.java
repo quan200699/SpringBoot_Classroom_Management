f203f62af090ea6f16aafde18e32f247f9dcfd5b
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-28 21:23:09 +0700

    add classroom controller

---
+++ src/main/java/com/codegym/classroom/controller/ClassroomController.java
@@ -0,0 +1,54 @@
+package com.codegym.classroom.controller;
+
+import com.codegym.classroom.model.Classroom;
+import com.codegym.classroom.model.Program;
+import com.codegym.classroom.service.classroom.IClassroomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/classrooms")
+public class ClassroomController {
+    @Autowired
+    private IClassroomService classroomService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Classroom>> getAllClassroom() {
+        return new ResponseEntity<>(classroomService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Classroom> createNewClassroom(@RequestBody Classroom classroom) {
+        return new ResponseEntity<>(classroomService.save(classroom), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Classroom> getClassroom(@PathVariable Long id) {
+        Optional<Classroom> classroomOptional = classroomService.findById(id);
+        return classroomOptional.map(classroom -> new ResponseEntity<>(classroom, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Classroom> updateClassroom(@PathVariable Long id, @RequestBody Classroom classroom) {
+        Optional<Classroom> classroomOptional = classroomService.findById(id);
+        return classroomOptional.map(classroom1 -> {
+            classroom.setId(classroom1.getId());
+            return new ResponseEntity<>(classroomService.save(classroom), HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Classroom> deleteClassroom(@PathVariable Long id) {
+        Optional<Classroom> classroomOptional = classroomService.findById(id);
+        return classroomOptional.map(classroom -> {
+            classroomService.remove(id);
+            return new ResponseEntity<>(classroom, HttpStatus.OK);
+        }).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+}
