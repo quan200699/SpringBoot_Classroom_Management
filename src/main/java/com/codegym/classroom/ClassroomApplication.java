0bba4ad42b5f79a468e92f72fad34000be144bc9
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-30 09:46:14 +0700

    fix post construct

--- src/main/java/com/codegym/classroom/ClassroomApplication.java
+++ src/main/java/com/codegym/classroom/ClassroomApplication.java
@@ -3,16 +3,14 @@ package com.codegym.classroom;
 import com.codegym.classroom.model.Job;
 import com.codegym.classroom.model.Language;
 import com.codegym.classroom.service.job.IJobService;
-import com.codegym.classroom.service.job.JobService;
 import com.codegym.classroom.service.language.ILanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
-import java.util.HashSet;
+
 import java.util.List;
-import java.util.Set;
 
 @SpringBootApplication
 public class ClassroomApplication {
@@ -36,20 +34,20 @@ public class ClassroomApplication {
             php.setName("PHP");
             languageService.save(php);
             Language asp = new Language();
-            php.setId(3L);
-            php.setName("ASP");
+            asp.setId(3L);
+            asp.setName("ASP");
             languageService.save(asp);
         }
         if (jobs.isEmpty()) {
             Job fullTime = new Job();
             fullTime.setName("Fulltime");
             jobService.save(fullTime);
-            Job partTime = new Job();
-            fullTime.setName("Partime sáng");
-            jobService.save(partTime);
-            partTime = new Job();
-            fullTime.setName("Partime chiều");
-            jobService.save(partTime);
+            Job partTime1 = new Job();
+            partTime1.setName("Partime sáng");
+            jobService.save(partTime1);
+            Job partTime2 = new Job();
+            partTime2.setName("Partime chiều");
+            jobService.save(partTime2);
         }
     }
 
