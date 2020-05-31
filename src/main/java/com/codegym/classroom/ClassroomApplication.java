5a9a887cb82326f29675ffa45a99b4a58c4b118a
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-31 16:54:09 +0700

    add auto create

--- src/main/java/com/codegym/classroom/ClassroomApplication.java
+++ src/main/java/com/codegym/classroom/ClassroomApplication.java
@@ -1,7 +1,9 @@
 package com.codegym.classroom;
 
+import com.codegym.classroom.model.Classroom;
 import com.codegym.classroom.model.Job;
 import com.codegym.classroom.model.Language;
+import com.codegym.classroom.service.classroom.IClassroomService;
 import com.codegym.classroom.service.job.IJobService;
 import com.codegym.classroom.service.language.ILanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +22,12 @@ public class ClassroomApplication {
     @Autowired
     private ILanguageService languageService;
 
+    @Autowired
+    private IClassroomService classroomService;
+
     @PostConstruct
     public void init() {
+        List<Classroom> classrooms = (List<Classroom>) classroomService.findAll();
         List<Job> jobs = (List<Job>) jobService.findAll();
         List<Language> languages = (List<Language>) languageService.findAll();
         if (languages.isEmpty()) {
@@ -48,6 +54,28 @@ public class ClassroomApplication {
             Job partTime2 = new Job();
             partTime2.setName("Partime chiều");
             jobService.save(partTime2);
+        }
+        if (classrooms.isEmpty()) {
+            Classroom classroom1 = new Classroom();
+            classroom1.setName("Tầng 3 TT01");
+            classroom1.setCapacity(49);
+            classroomService.save(classroom1);
+            Classroom classroom2 = new Classroom();
+            classroom2.setName("Tầng 4 TT01");
+            classroom2.setCapacity(45);
+            classroomService.save(classroom2);
+            Classroom classroom3 = new Classroom();
+            classroom3.setName("Tầng 1 TT04");
+            classroom3.setCapacity(36);
+            classroomService.save(classroom3);
+            Classroom classroom4 = new Classroom();
+            classroom4.setName("Tầng 2 TT04");
+            classroom4.setCapacity(30);
+            classroomService.save(classroom4);
+            Classroom classroom5 = new Classroom();
+            classroom5.setName("Tầng 3 TT04");
+            classroom5.setCapacity(36);
+            classroomService.save(classroom5);
         }
     }
 
